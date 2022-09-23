a2ce27b1c701cc314729d2edfa7e0eace9745e9a
JoonPyoJang <jjpyo1009@gmail.com>
2022-09-23 12:13:18 +0900

    장고 로그인 및 회원가입 정보 추가
    로그인이 성공 했을떄 로그인하는 username을 로그인 성공 글자 앞에 띄움, 회원 가입때 중복이 있을때 예외처리

--- user/views.py
+++ user/views.py
@@ -1,3 +1,5 @@
+from lib2to3.pgen2 import driver
+from ssl import AlertDescription
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
@@ -15,17 +17,22 @@ def sign_up_view(request):
         password2 = request.POST.get('password2',None)
         bio = request.POST.get('bio',None)
 
+        exist_username = UserModel.objects.filter(username=username)
+        
         if password != password2:
             return render(request, 'user/signup.html')
-        else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.password2 = password2
-            new_user.bio = bio
-            new_user.save()
 
-        return redirect('/sign-in')
+        else:
+            if exist_username:
+                return render(request, "user/signup.html")
+            else:
+                new_user = UserModel()
+                new_user.username = username
+                new_user.password = password
+                new_user.password2 = password2
+                new_user.bio = bio
+                new_user.save()
+                return redirect('/sign-in')
     
 
 
@@ -35,10 +42,13 @@ def sign_in_view(request):
         password = request.POST.get('password',None)
 
         me = UserModel.objects.get(username=username)
+            
+        
         if me.password == password:
-            request.sesstion['user'] = me.username
-            return HttpResponse("로그인 성공!")
+            request.session['user'] = me.username
+            return HttpResponse(f"{username} 로그인 성공!")
         else:
             return redirect('/sign-in')
+
     elif request.method == 'GET':
-     return render(request, 'user/signin.html')
+        return render(request, 'user/signin.html')
